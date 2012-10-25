305e74ac71b75e92556fcf7f1fbf6529cb10e2e1
Evan Villemez <evan.villemez@americancouncils.org>
2012-10-25 09:49:57 -0400

    updated to transcoding 0.3.0, added status command, and sonata/rabbitmq integration

---
+++ Command/StatusCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace AC\TranscodingBundle\Command;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+use AC\TranscodingBundle\Console\OutputSubscriber;
+use AC\Component\Transcoding\File;
+use AC\Component\Transcoding\Transcoder;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+
+
+class StatusCommand extends ContainerAwareCommand
+{
+    
+    protected function configure()
+    {
+        $this->setName("transcoder:status")->setDescription("Show config, status of all available adapters, and list usable presets.");
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $formatter = $this->getHelper('formatter');
+
+        //show adapters
+        $adapters = $this->getContainer()->get('transcoder')->getAdapters();
+        $output->writeln("Adapter status:");
+        if (empty($adapters)) {
+            $output->writeln($formatter->formatBlock("No available adapters.", 'info'));
+        } else {
+            foreach ($adapters as $adapter) {
+                $msg = $adapter->verify() ? "Verified." : $adapter->getVerificationError();
+                $msg = $adapter->verify() ? $formatter->formatBlock($msg, 'info') : $formatter->formatBlock($msg, 'error');
+                $output->writeln($formatter->formatBlock($adapter->getName().":      ", 'comment').$msg);
+            }
+        }
+
+        //show presets
+        $output->writeln('');
+        $output->writeln("Usable Presets: ");
+
+        $presets = $this->getContainer()->get('transcoder')->getPresets();
+        $usablePresets = array();
+        foreach ($presets as $preset) {
+            if ($this->getContainer()->get('transcoder')->getAdapter($preset->getRequiredAdapter())->verify()) {
+                $usablePresets[] = $preset;
+            }
+        }
+
+        if (empty($usablePresets)) {
+            $output->writeln($formatter->formatBlock("No usable presets.", 'info'));
+        } else {
+            foreach ($usablePresets as $preset) {
+                $output->writeln($formatter->formatBlock($preset->getName()." (".$preset->getKey().")", 'comment').": ".$formatter->formatBlock($preset->getDescription(), 'info'));
+            }
+        }
+
+        return true;
+    }
+}
--- DependencyInjection/RegisterTranscoderTagsPass.php
+++ DependencyInjection/RegisterTranscoderTagsPass.php
@@ -14,16 +14,53 @@ class RegisterTranscoderTagsPass implements CompilerPassInterface
             return;
         }
 
-        $definition = $container->getDefinition('transcoder');
+        $transcoderDefinition = $container->getDefinition('transcoder');
 		
-		foreach (array('adapter','preset','job', 'listener') as $tagType) {
-			$tagName = 'transcoder.'.$tagType;
+        //register adapters/presets
+		foreach (array('adapter','preset') as $tagType) {
+			$tagName = 'transcoding.'.$tagType;
 			$method = 'register'.ucfirst($tagType);
 			
 			//call registration method
 			foreach ($container->findTaggedServiceIds($tagName) as $id => $attributes) {
-				$definition->addMethodCall($method, array(new Reference($id)));
+				$transcoderDefinition->addMethodCall($method, array(new Reference($id)));
 			}
 		}
+        
+        $dispatcherDefinition = $container->getDefinition('event_dispatcher');
+        
+        //register event listeners
+        foreach ($container->findTaggedServiceIds('transcoding.listener') as $id => $events) {
+            foreach ($events as $event) {
+                $priority = isset($event['priority']) ? $event['priority'] : 0;
+
+                if (!isset($event['event'])) {
+                    throw new \InvalidArgumentException(sprintf('Service "%s" must define the "event" attribute on "transcoding.listener" tags.', $id));
+                }
+
+                if (!isset($event['method'])) {
+                    $event['method'] = 'on'.preg_replace(array(
+                        '/(?<=\b)[a-z]/ie',
+                        '/[^a-z0-9]/i'
+                    ), array('strtoupper("\\0")', ''), $event['event']);
+                }
+
+                $dispatcherDefinition->addMethodCall('addListenerService', array($event['event'], array($id, $event['method']), $priority));
+            }
+        }
+        
+        //register event subscribers
+        foreach ($container->findTaggedServiceIds('transcoding.subscriber') as $id => $attributes) {
+            // We must assume that the class value has been correcly filled, even if the service is created by a factory
+            $class = $container->getDefinition($id)->getClass();
+
+            $refClass = new \ReflectionClass($class);
+            $interface = 'Symfony\Component\EventDispatcher\EventSubscriberInterface';
+            if (!$refClass->implementsInterface($interface)) {
+                throw new \InvalidArgumentException(sprintf('Service "%s" must implement interface "%s".', $id, $interface));
+            }
+
+            $dispatcherDefinition->addMethodCall('addSubscriberService', array($id, $class));
+        }
     }
 }
